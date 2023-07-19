64a02dc8721724b6789dd681112c8e6cbd7b0831
AsifKhan <Pathanasif0003@gmail.com>
2023-07-19 11:47:44 +0530

    commited

--- DBUtils.java
+++ DBUtils.java
@@ -2,3 +2,46 @@ I am in development branch....
 I am updating this file
 Hello Guys
 this file contains DB related files.
+[33mcommit 1f524a62cdedace1380c5193a8276028b29d4d30[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;32mdevelopment[m[33m)[m
+Author: AsifKhan <Pathanasif0003@gmail.com>
+Date:   Wed Jul 19 11:27:31 2023 +0530
+
+    this commit is done from development branch
+
+[33mcommit 5bac7ef6a825b9ce9067b376e4286e153be6761a[m
+Author: AsifKhan <Pathanasif0003@gmail.com>
+Date:   Wed Jul 19 10:57:48 2023 +0530
+
+    Revert "this is reverted the classs"
+    
+    This reverts commit c9d7e04665d89e386f8d4a5eca4fdd5e9f76c3e6.
+
+[33mcommit 51a7b225ec348381009a973cfa26ecea96afc471[m[33m ([m[1;31mfb/master[m[33m)[m
+Author: AsifKhan <Pathanasif0003@gmail.com>
+Date:   Wed Jul 19 10:52:29 2023 +0530
+
+    this is updated file
+
+[33mcommit d09eacdcd5c8b154b883cf2346065c86e05d80f2[m
+Author: AsifKhan <Pathanasif0003@gmail.com>
+Date:   Tue Jul 18 15:49:50 2023 +0530
+
+    this script will remove file
+
+[33mcommit 2fa68eab357f57b5a2869324fc7fa54d942f6971[m
+Author: AsifKhan <Pathanasif0003@gmail.com>
+Date:   Tue Jul 18 15:45:54 2023 +0530
+
+    updated file..
+
+[33mcommit c9d7e04665d89e386f8d4a5eca4fdd5e9f76c3e6[m
+Author: AsifKhan <Pathanasif0003@gmail.com>
+Date:   Tue Jul 18 14:01:06 2023 +0530
+
+    Added Service Class
+
+[33mcommit c7d32c853e495a2398a171dec3301e4376032698[m
+Author: AsifKhan <Pathanasif0003@gmail.com>
+Date:   Tue Jul 18 13:59:45 2023 +0530
+
+    First commit
--- StringUtils.java
+++ StringUtils.java
@@ -1,3 +1,7 @@
-this file updated from the vi editor 
+this is master branch line
+
+
+this file updated from the vi editor
+
 
 This is java class which contains string related funtion
